7738fb23895599a1838ff1582716e7e420099fd0
dev2325 <arnab.banerjee@triconinfotech.com>
2019-08-05 10:53:29 +0530

    This is assessment of react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import { Users } from './users/users';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Posts } from './posts/posts';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      // <HomePage/>
+      <Router>
+        <div style={{textAlign:"justify"}}>
+          <ul>
+
+            <li>
+              <Link to="/posts">Posts</Link>
+            </li>
+
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+
+          </ul>
+
+          <hr />
+
+
+          <Route path="/posts" component={Posts} />
+          <Route path="/users" component={Users} />
+
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
