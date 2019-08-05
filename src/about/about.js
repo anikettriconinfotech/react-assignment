bb3da60ba2b864e89d2b3fa4b7aee80d00223879
kamal-tricon <53036948+kamal-tricon@users.noreply.github.com>
2019-08-05 10:12:22 +0530

    add clock done

--- src/about/about.js
+++ src/about/about.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
-export class About extends React.Component{
-    render(){
+export class About extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            currentTime: ''
+        }
+    }
+
+    startClock = () => {
+       var  interval = setInterval(this.getCurrentTime, 500);
+
+        
+    }
+
+    getCurrentTime = () => {
+        var curretTime = new Date();
+
+        var hours = curretTime.getHours();
+        var minutes = curretTime.getMinutes();
+        var seconds = curretTime.getSeconds();
+
+        minutes = (minutes < 10 ? '0' + minutes : minutes);
+        seconds = (seconds < 10 ? '0' + seconds : seconds);
+
+        this.setState({
+            currentTime: hours + ':' + minutes + ':' + seconds
+        })
+    }
+    render() {
         return (
-            <div className="about">This is about section</div>
+            <div className="about">This is about section
+                <button onClick={this.startClock}>Start Clock</button>
+                <p>Time is : {this.state.currentTime}</p>
+            </div>
+
         );
     }
 }
