0cecdca9b3d9963fcdbe90d8cea29e36fb2eb35f
Ravindra Nath <ravindra.nath@Triconinfotech.com>
2019-08-05 10:07:42 +0530

    Assignment is half completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
 import React from 'react';
 import logo from './logo.svg';
+import {HomePage} from './homepage/HomePage'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+
+            items: result.items
+          });
+        });
+  
+  }
+  render(){
+    return(
+      <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+      </div>
+    </Router>
+
+
+
+    );
+    
+  function Home() {
+    return (
+      <div>
+        <h2>Home</h2>
+      </div>
+    );
+  }
+  
+  function About() {
+    return (
+      <div>
+        <h2>About</h2>
+      </div>
+    );
+  }
+
+  function Topics() {
+    return (
+      <div>
+        <h2>topics</h2>
+        <button>Show Post</button>
+      <div>  </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+}    export default App;
---
+++ src/homepage/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {} from './mainbar';
+import {} from './sidebar';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+export class HomePage extends React.Component {
+    render() {
+      return <div className='Homepage'>Hello</div>;
+    }
+  }
